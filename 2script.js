641d6494f8a1e7a014695d036e8d4923b9df9664
erynebustarret <eryne.bustarret@gmail.com>
2025-03-16 20:51:01 +0100

    Update 2script.js

--- 2script.js
+++ 2script.js
@@ -7,3 +7,23 @@ explanationBox.addEventListener('click', () => {
     // Ouvrir ou fermer la boîte explicative en ajoutant ou retirant la classe 'open'
     explanationBox.classList.toggle('open');
 });
+
+    function changeLanguage(lang) {
+      // Change l'attribut lang dans la balise <html>
+      document.documentElement.lang = lang;
+
+      // Change le contenu de la page en fonction de la langue
+      const text = {
+        fr: {
+          title: "Plonge tes doigts dans la soupe",
+          content: "ceci est en francais"
+        },
+        en: {
+          title: "Dip your fingers in the soup",
+          content: "This is in English"
+        }
+      };
+
+      document.getElementById("title").innerText = text[lang].title;
+      document.getElementById("content").innerText = text[lang].content;
+    }
