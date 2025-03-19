a4ed8aeea0245d20c22dab99b674c84bd441a6c9
erynebustarret <eryne.bustarret@gmail.com>
2025-03-19 09:18:36 +0100

    Update 2script.js

--- 2script.js
+++ 2script.js
@@ -15,12 +15,21 @@ explanationBox.addEventListener('click', () => {
       // Change le contenu de la page en fonction de la langue
       const text = {
         fr: {
-          title: "Plonge tes doigts dans la soupe",
-          content: "ceci est en francais"
+            title: "Plonge tes doigts dans la soupe",
+            list: "Click sur les objets qui apparaissent, Étudies les éléments auquels ils renvoient",
+            content: "Tentative de retranscription de mon processus créatif. J'aime beaucoup l'idée d'un sorte de vide poche à idées. Je réflechis constamment entourée de petit objets, je fais parfois des croquis mais peu, ils sont purement techniques. Ici, je me questionne sur mes chemins de réflexions plus que la façon dont je réflechis.  Ainsi, il me semble plus juste d'aborder ce travail comme un jeu, un paysage avec pleins de chemins et de neuds.",
+            content:" Les différentes images viennent d'objets que j'ai personnellement glanés, puis scanés, ou des photographies avec mon tel. Ceci, auxquelles s'accumulent des références visuelles récoltées sur toutes sortes de plateforme (sources complètes dans la mesure du possible). ",
+            footer:"(Soyez indulgents avec mes comptépences informatiques. J'apprends.)",
+            footer:"Eryne Bustarret, 2025",
+            
         },
         en: {
-          title: "Dip your fingers in the soup",
-          content: "This is in English"
+            title: "Dip your fingers in the soup",
+            list: "Click sur les objets qui apparaissent, Étudies les éléments auquels ils renvoient",
+            content: "Tentative de retranscription de mon processus créatif. J'aime beaucoup l'idée d'un sorte de vide poche à idées. Je réflechis constamment entourée de petit objets, je fais parfois des croquis mais peu, ils sont purement techniques. Ici, je me questionne sur mes chemins de réflexions plus que la façon dont je réflechis.  Ainsi, il me semble plus juste d'aborder ce travail comme un jeu, un paysage avec pleins de chemins et de neuds.",
+            content:" Les différentes images viennent d'objets que j'ai personnellement glanés, puis scanés, ou des photographies avec mon tel. Ceci, auxquelles s'accumulent des références visuelles récoltées sur toutes sortes de plateforme (sources complètes dans la mesure du possible). ",
+            footer:"(Soyez indulgents avec mes comptépences informatiques. J'apprends.)",
+            footer:"Eryne Bustarret, 2025",
         }
       };
 
